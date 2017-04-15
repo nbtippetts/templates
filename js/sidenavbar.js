9ce70046954569ea6cffd0ccbf31ddf0e13a64b2
Noah Tippetts <nbtippetts@gmail.com>
2017-04-14 20:56:48 -0600

    input

---
+++ js/sidenavbar.js
@@ -0,0 +1,79 @@
+function phoneFormat(input){
+        // Strip all characters from the input except digits
+        input = input.replace(/\D/g,'');
+
+        // Trim the remaining input to ten characters, to preserve phone number format
+        input = input.substring(0,10);
+        var phoneNumber = document.getElementById('phoneNumber');
+        if(input.charAt(0) == 0 || input == 0){
+                phoneNumber.style.color = "red";
+              //  document.write('Invaild Number')
+        } else {
+                phoneNumber.style.color = "black";
+                //document.write('Vaild Number')
+        }
+
+        // Based upon the length of the string, we add formatting as necessary
+        var size = input.length;
+        if (size == 0) {
+                input = input
+        } else if(size < 4){
+                input = '('+input;
+        } else if(size < 7){
+                input = '('+input.substring(0,3)+') '+input.substring(3,6);
+        } else{
+                input = '('+input.substring(0,3)+') '+input.substring(3,6)+' - '+input.substring(6,10);
+        }
+        return input;
+   }
+   document.getElementById('phoneNumber').addEventListener('keyup',function(evt){
+        //var phoneNumber = document.getElementById('phoneNumber');
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        phoneNumber.value = phoneFormat(phoneNumber.value);
+
+   });
+// const sideNavBar = document.querySelector(".side-navbar");
+// const rightArrow = document.getElementById('right-arrow');
+// const leftArrow = document.getElementById('left-arrow');
+// //const sideLogo = document.querySelector(".side-logo");
+// //const sideNavBar = document.querySelector(".side-navbar");
+// // function openNav() {
+// //   sideNavBar.style.width = '135px';
+// //   rightArrow.style.display = 'block';
+// //   leftArrow.style.display = 'none';
+// // }
+// //
+// // function closeNav() {
+// //   sideNavBar.style.width = '400px';
+// //   rightArrow.style.display = 'none';
+// //   leftArrow.style.display = 'block';
+// // }
+// document.addEventListener('click', leftArrow, function() {
+//   sideNavBar.style.width = '135px';
+//   rightArrow.style.display = 'block';
+//   leftArrow.style.display = 'none';
+// });
+// document.addEventListener('click', rightArrow, function() {
+//   sideNavBar.style.width = '400px';
+//   rightArrow.style.display = 'none';
+//   leftArrow.style.display = 'block';
+// });
+//  //var open = document.addEventListener('click', openNav);
+// //console.log(closeNav)
+// $(document).ready({
+//   $('#right-arrow').on('click', function() {
+//     $('#right-arrow').hide();
+//     $('#left-arrow-arrow').show();
+//     $('.side-navbar').css('width', '400px')
+//   })
+//})
+
+// function phoneNumber(input) {
+//    input = input.replace(/\D/g,'');
+//    input = input.substring(0,10);
+//    var input = input.length;
+//    if (input == 0) {
+//
+//    }
+// }
+//
